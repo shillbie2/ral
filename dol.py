bb762f6b39c9a26e434347a76a52d25658edf5dd
shillbie2 <37108416+shillbie2@users.noreply.github.com>
2018-03-07 07:52:01 -0800

    Add files via upload

---
+++ dol.py
@@ -0,0 +1,64 @@
+from RAbotproses import LineBot
+
+import threading
+
+
+def login(resp, auth):
+
+	bot = LineBot(resp, auth)
+
+
+Dol = threading.Thread(target=login, args=('Dol','EqfnyDroeUtVRYVzCqJe.ZyIijJ0iuSPM1n+hcx8+JG.9OXfbhghgniiHSH+h4Zrywt4V6waAo5Dq55IQndOcuc=')).start()
+
+Dol2 = threading.Thread(target=login, args=('Dol2','Eq3sM4HNutKMRV82Off9.beSI/oCf3qHqjJ3W6S5rkq.KzlMLixVQQRXhxGoyngNHGp/Yta+9jMOiQbsahosdpg=')).start()
+
+Dol3 = threading.Thread(target=login, args=('Dol3','EqOAW6CLgvt3x4H2jxV5.hCBYhI9aBK+qwilS+q/KXq.OYQ20LxmOfv85TRmHgY/hfNdVAjxX5kVUGInE3a9e9Q=')).start()
+
+Dol4 = threading.Thread(target=login, args=('Dol4','Eqrh6V2n4J0jvuSuXp43.OdYm6+QTvccurtz5zba4aW.m9pmdXSgiru/VxBJshIMMSKgFZ+0CSTLY4+o2Kc5Yws=')).start()
+
+Dol5 = threading.Thread(target=login, args=('Dol5','EqT6R7IJzbYaTqRV0pta.gJuO6JQwaodXAr6dow8AEG.QnqW7oLx7ImMlWafjVGNQaud627fihiRumfNuRYULss=')).start()
+
+Dol6 = threading.Thread(target=login, args=('Dol6','EqVUh5dM2ktMzfyzYir0.NpBK6nc/CHkueSAzvEygua.MilGFsQ0jH22VnHN48IX0+EsoFg2CXtACn1IVgybtc0=')).start()
+
+Dol7 = threading.Thread(target=login, args=('Dol7','EqPTYrOXadEMDsxI5Hef.HhcJ7540YTm2+i7ItHXzhW.oTsWNxOMpNRy3qCPS+CHCJDX+GlBbKXsf1doI3kYTeI=')).start()
+
+Dol8 = threading.Thread(target=login, args=('Dol8','EqxFIhLnNyhK4nQJfpm1.71gbM/i2pqRjcurUsZQXSq.YZBEzGDoc8MJHrexHIB+z9GtM/AUBopKCvBsBvV1ZRg=')).start()
+
+Dol9 = threading.Thread(target=login, args=('Dol9','Eqf6KIra2Ui1rDHOGOL3.L18AnxPjsgbRF8YzF0A4OW.bQ4Qq4/90jD1k8L6nmTKXXELotMmF+jF+JLdFEA8UI0=')).start()
+
+Dol10 = threading.Thread(target=login, args=('Dol10','EqHRvq6kY3lIAvPgk4Ha.yhTZoqDumxUp5mO/cE6owG.l9cdtthWcsuxcZSbq4E6ESyW4du34dr/UTs1+9pIHiQ=')).start()
+
+Dol11 = threading.Thread(target=login, args=('Dol11','EqnyuUlvcAikBLPvNFUd.uVrr3VvLWBjP+BWR+qQp7q.RPlS8cI21PuvIP6dSsWKKYYiextGR6Kz1Z9HvJWcA+Y=')).start()
+
+Dol12 = threading.Thread(target=login, args=('Dol12','EqeNExvv4bYgCXWDppj0.50XYbzUa7LKSck7PTTWwOa.aDonCDOXouy8BQg8wHKeus2tI5QPnX5yuxpjDNslk+g=')).start()
+
+Dol13 = threading.Thread(target=login, args=('Dol13','EqD5hXGI8S1hJc57IbI3.pY1Ri6VEvU2/Z4xKdKmpOW.q5lMtUGLbbhkLvNcf+dSJeGGDWcIG9ebD7K7yyuF03k=')).start()
+
+Dol14 = threading.Thread(target=login, args=('Dol14','Eq2eHfxPpTva86K3lLI5.MIBFhIrgsiTsN/VS8zryfq.mEGNXC4cquDJiyEDy7LvnoO2/Y5k3zjCxGbM6QMVnK0=')).start()
+
+Dol15 = threading.Thread(target=login, args=('Dol15','EqO6vSwtzot1lOe4oAw6.on1M6KMmxoL39eaCwBF2rG.9R0pWJLYF3Sg4mHUj8d7DXDvO6cAq8dR82s0WiKSdAI=')).start()
+
+Dol16 = threading.Thread(target=login, args=('Dol16','EqCs0NmVITeZayzbWYH3.TykfI//Ig3TOL/IiwLmFSW.Q95CHM0Be1LLngBMG+QlyLBtp7lUyuIhuCxbUL4VxYc=')).start()
+
+Dol17 = threading.Thread(target=login, args=('Dol17','EquXMTPOmeGAt44u1Vrd.G3MH8TbO437VYr5eFeCStq./Bo8IbQlWi+UbEpcPUZDoL7R9HZDgzwFc2wgXuKQrtM=')).start()
+
+Dol18 = threading.Thread(target=login, args=('Dol18','EqynBQYoXkAcXjTR1Eq6.7ng+HqEwNkzm7s1rFjqxPG.56gmGfm4hpywSxisX1JR1OtBq1Wb0ceQXej4YflDpaE=')).start()
+
+Dol19 = threading.Thread(target=login, args=('Dol19','Eq2blR51fDjG1DbhDZue.wagZ22UQsrbRY2C1ff+tBG.h2OlZMtU1BwjBo/hLopWwbjOj1UzXUAqciQQwNk5nY8=')).start()
+
+Dol20 = threading.Thread(target=login, args=('Dol20','EqpmuVYskaaWidqRnqj0.jx1X8+4/VPFW2FdMsXN3Ka.OzcSt+lEQpMYRcj6ZO7q39yIe+vUw2XdznGStHAuc1g=')).start()
+
+Dol21 = threading.Thread(target=login, args=('Dol21','EqUrYOxPYLdUv1Cep8Ea.fL/FM9D5a4azb7cvC8E9wG.bh9mvHrC2JJ1pYLqAf4Xw2ZRcjKpwcoeftJZtS1cpJo=')).start()
+
+Dol22 = threading.Thread(target=login, args=('Dol22','EqKJiSaS8TP1R5OxBVl7.edYNVGnGu2VzNpvuGHJNbW.IdpszGjHNfio8LNn4NmPbcbh9rgAh0FqcV8PQxCtWPU=')).start()
+
+Dol23 = threading.Thread(target=login, args=('Dol23','EqyKb4yeRfb2kYKg9Lz8.uJrXzGp4xoAf8MXasxTVoa.cdqwDhCFR1mkh+XOYK1H1fXaKAa3iUT8BwgNJLuKD3k=')).start()
+
+Dol24 = threading.Thread(target=login, args=('Dol24','EqUg7l9G01goLDqfAuw0.1U6KsqNqyFsYGBJA97bUaa.aylGvFip5f+p74Hm68cRR6T88Md1BhkvSF4DsvsWyNI=')).start()
+
+Dol25 = threading.Thread(target=login, args=('Dol25','EqPAQLCEVAFwtMZsMQWb.rKdr/2WyUJYmSIu//JGqAW.pj+Lfrhzfwl1D9J8lSO25XSuoglWapGJ3V1YZqzLS/g=')).start()
+
+Dolsatpam = threading.Thread(target=login, args=('Dolsatpam','EqyPn2FQWHttqlT5v0Ld.KxCOGzZfnFWUyjzuP3OPtq.yPdExNDP+2POJdHzE4rlzo3pjnGwhja1D1UN92+BkyI=')).start()
+
+
+print('Login Berhasil!')
